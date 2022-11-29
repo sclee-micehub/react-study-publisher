43abd5e6405bc98cf303d0c992dd44ca13cf8971
seungcheol <sc.lee@micehub.com>
2022-11-29 09:59:45 +0900

    Update: 스터디 #1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => <h1>Hello World!</h1>
+
+const App = () => (
+    <div>
+        App
+        <HelloWorld />
+    </div>
+);
+
+export default App
