5b3316aef3af7792b9ce724a8256754d6a5a3b93
seungcheol <sc.lee@micehub.com>
2022-11-30 14:54:08 +0900

    Test: #2 CSS 모듈 + SCSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import StyleCard from "./components/StyleCard";
+import CssModuleCard from "./components/CssModuleCard";
 
 const App = function () {
   return (
     <div>
       <StyleCard />
+      <CssModuleCard />
     </div>
   );
 };
--- src/components/CssModuleCard.js
+++ src/components/CssModuleCard.js
@@ -1,41 +1,38 @@
 import React from "react";
 import { AiOutlineHeart } from "react-icons/ai";
-import styles from "./StyleCard.styles";
+import classes from "./CssModuleCard.module.scss";
 
-const StyleCard = () => (
-  <div className="card" style={styles.box}>
-    <figure style={styles.figure}>
+console.log(classes);
+
+const CssModuleCard = () => (
+  <div className={classes.box}>
+    <figure>
       <img
         src="https://googlefonts.github.io/korean/public/assets/og-img.png"
         alt="Google Fonts + 한국어"
-        style={styles.img}
       />
     </figure>
-    <div className="title" style={styles.title}>
-      <h3 style={styles.titleText}>Google Fonts + 한국어</h3>
-      <span className="chip" style={styles.titleCategory}>
-        OSS
-      </span>
+    <div className={classes.title}>
+      <h3>Google Fonts + 한국어</h3>
+      <span className={classes.chip}>OSS</span>
     </div>
-    <p className="summary" style={styles.summary}>
+    <p className={classes.summary}>
       좋은 타이포그래피를 통해 웹은 더욱 아름답고, 빠르며, 누구나 참여할 수 있는 공간이 될 수
       있습니다. Google Fonts는 이 사이트에 수록된 오픈 소스 한글 폰트를 머신 러닝에 기반을 둔 최적화
       기술을 통해 시범적으로 제공합니다.
     </p>
-    <div className="footer" style={styles.footer}>
+    <div className={classes.footer}>
       <a
+        className={`${classes.button} ${classes.primary} ${classes["btn-details"]}`}
         href="https://googlefonts.github.io/korean/"
-        className="button primary btn-details"
         target="_blank"
-        style={styles.btnDetails}
         rel="noreferrer"
       >
         자세히 보기
       </a>
       <button
-        type="bytton"
-        className="button default btn-favorite"
-        style={styles.btnFavorite}
+        type="button"
+        className={`${classes.button} ${classes.default} ${classes["btn-favorite"]}`}
         title="좋아요"
       >
         <AiOutlineHeart />
@@ -44,4 +41,4 @@ const StyleCard = () => (
   </div>
 );
 
-export default StyleCard;
+export default CssModuleCard;
